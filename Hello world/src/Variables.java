c00d409d92d556eff5126f2e991b1938798d8865
AYSEGUL AKTAS <4ysglakts@gmail.com>
2024-02-11 13:22:54 +0300

    variables commit

---
+++ Hello world/src/Variables.java
@@ -0,0 +1,23 @@
+public class Variables {
+
+    public static void main(String[] args) {
+
+     String name,call;
+     int age ,average,num2 ,num3;
+     name ="Aysegul";
+     age=22;
+     double num1=2.5;
+     num2=456;
+     num3=44;
+
+
+     average= (num3+num2)/2;
+     call=name+" " +age;
+     num1=age*num1;
+
+
+     System.out.println(call);
+     System.out.println(num1);
+     System.out.println("average:" +average);
+    }
+}
