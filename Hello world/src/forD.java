6da82341e2b037aca8b3d37e69d59247fcb8b3a1
AYSEGUL AKTAS <4ysglakts@gmail.com>
2024-02-12 15:32:22 +0300

    loops -for

---
+++ Hello world/src/forD.java
@@ -0,0 +1,23 @@
+public class forD {
+    public static void main(String[] args) {
+
+        int n=10;
+        int toplam=0;
+        int toplamC=0;
+        int ToplamT=0;
+
+
+        for (int i = 1; i<=n; i++){
+          if (i%2==0) toplamC=toplamC +i;
+          else ToplamT=ToplamT +i;
+
+        }
+        System.out.println("sayıla:"+ toplam);
+        System.out.println("ciftler toplamı:"+toplamC);
+        System.out.println("tekler toplamı:" +ToplamT );
+    }
+}
+
+
+
+
