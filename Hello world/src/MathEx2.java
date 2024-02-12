486b6f60ac0c8266ad2638ebd4153de35f0c5504
AYSEGUL AKTAS <4ysglakts@gmail.com>
2024-02-12 14:12:40 +0300

    mod alma ve tip dönüsümü

--- Hello world/src/MathEx2.java
+++ Hello world/src/MathEx2.java
@@ -1,3 +1,5 @@
+import com.sun.jdi.connect.Connector;
+
 public class MathEx2 {
     public static void main(String[] args) {
 
@@ -7,6 +9,18 @@ public class MathEx2 {
         int b=8;
         int bolm=a%b;
         System.out.println("kalan: "+ bolm);
+
+
+    // integer veriyi strınge cevırme
+
+        String str= String.valueOf(a);
+        System.out.println("str : " +str);
+
+        str=str.substring(1,2)+str.substring(0,1);
+        System.out.println("str: "+str);
+
+       
+
 
 
 
