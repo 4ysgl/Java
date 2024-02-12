545a1746ce44dad27f7cb30eda92ec11879ecf1a
AYSEGUL AKTAS <4ysglakts@gmail.com>
2024-02-12 14:35:28 +0300

    If else yapısı

---
+++ Hello world/src/IfElse.java
@@ -0,0 +1,24 @@
+public class IfElse {
+
+    public static void main(String[] args) {
+        int a,b;
+
+        a=-5;
+        b=47;
+        if (a>0){
+
+            System.out.println("a sayısı pozıtıf ");
+        }
+
+        else if (a<0){
+            System.out.println("a sayısı negatıf");
+        }
+        else {
+            System.out.println("a =0");
+
+        }
+
+
+
+    }
+}
--- Hello world/src/MathEx2.java
+++ Hello world/src/MathEx2.java
@@ -19,7 +19,7 @@ public class MathEx2 {
         str=str.substring(1,2)+str.substring(0,1);
         System.out.println("str: "+str);
 
-       
+
 
 
 
