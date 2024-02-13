bffae61bccead06824dfb4bcce9fe766b45e5eeb
AYSEGUL AKTAS <4ysglakts@gmail.com>
2024-02-13 16:12:55 +0300

    while Loops

---
+++ Hello world/src/whileloops.java
@@ -0,0 +1,17 @@
+public class whileloops {
+    public static void main(String[] args) {
+        //while dongusu kosul saglandıgı surece devam eder
+        int i=0;
+
+        while (i<10){
+            i++;
+
+            System.out.println("sayılar: "+i);
+        }
+
+
+
+
+
+    }
+}
