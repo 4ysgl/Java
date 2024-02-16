70f3841bf89e658366885b04a2c2bd9b56825778
AYSEGUL AKTAS <4ysglakts@gmail.com>
2024-02-16 14:42:32 +0300

    Arrays içerisinde en buyuk ve en kck sayiyi getirme ve sırasını yazdırma

--- Hello world/src/Arrays3.java
+++ Hello world/src/Arrays3.java
@@ -4,24 +4,28 @@ public class Arrays3 {
         // dızı ıcerısınde en buyuk ve en kck sayıyı bulamk ıcın for ve ıf kullanıldı
 
 
-        int [] BykSa=new int[]{15,41,12,52,12,75};
+        int [] BykSa=new int[]{15,41,12,52,18,75};
         int EnByk=BykSa[0];
         int EnKck=BykSa[0];
+        int sirak=0,sirab = 0;
         for (int i=0;i<BykSa.length;i++)
         {
             if (EnByk<BykSa[i])
             {EnByk=BykSa[i];
+                sirab=i;
             }
-            else if(EnByk>BykSa[i])
+            else if(EnByk>BykSa[i])   //burada kucuk olanın sırasını da eklemesını ıstıyorum ama 4. ındex gelıyor
             {
                EnKck =BykSa[i];
+                sirak=i;
 
             }
 
         }
 
-        System.out.println("en buyuk sayı:" +EnByk);
-        System.out.println("en kucuk sayı:" +EnKck);
+        System.out.println("en buyuk sayı:" +EnByk   +" sırası: "+sirab);
+        System.out.println("en kucuk sayı:" +EnKck+" sırası: "+sirak);
+
 
     }
 }
