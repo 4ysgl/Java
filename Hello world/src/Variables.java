0b138d4f88f669b70ef9085cfe0174be8c4aa38e
AYSEGUL AKTAS <4ysglakts@gmail.com>
2024-09-01 00:18:05 +0300

    variables tekrar

--- Hello world/src/Variables.java
+++ Hello world/src/Variables.java
@@ -2,22 +2,30 @@ public class Variables {
 
     public static void main(String[] args) {
 
-     String name,call;
-     int age ,average,num2 ,num3;
-     name ="Aysegul";
-     age=22;
-     double num1=2.5;
-     num2=456;
-     num3=44;
+//     String name,call;
+//     int age ,top,num2 ,num3;
+//     name ="Aysegul";
+//     age=22;
+//     double num1=2.5;
+//     num2=456;
+//     num3=44;
+//
+//
+//     top= (num3+num2)/2;
+//     call=name+" " +age;
+//     num1=age*num1;
+//
+//
+//     System.out.println(call);
+//     System.out.println(num1);
+//     System.out.println("average:" +top);
 
+     // variables
 
-     average= (num3+num2)/2;
-     call=name+" " +age;
-     num1=age*num1;
+     String name ="aa" ;
+     int age=88,top,age2=22;
+     top=age-age2;
+        System.out.println(name +  " " +  "kisisi sonuc :" +top);
 
-
-     System.out.println(call);
-     System.out.println(num1);
-     System.out.println("average:" +average);
     }
 }
