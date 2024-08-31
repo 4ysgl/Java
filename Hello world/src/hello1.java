1e5775690f90d38122a05c1d9fb74b1fbcbe4c9e
AYSEGUL AKTAS <4ysglakts@gmail.com>
2024-09-01 00:02:00 +0300

    ı come back :)

--- Hello world/src/hello1.java
+++ Hello world/src/hello1.java
@@ -1,5 +1,10 @@
 public class hello1 {
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         System.out.println( "hello world new message for this pr.");
+
+        System.out.println("hello everyone I come back :))");
     }
+
+
 }
